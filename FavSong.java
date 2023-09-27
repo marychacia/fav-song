04c395d3e37873aad43614df67f60f6eb4fe12e4
Mary Chacia <129899229+marychacia@users.noreply.github.com>
2023-09-27 10:47:36 +0100

    Add files via upload

---
+++ FavSong.java
@@ -0,0 +1,71 @@
+/* Favourite Song metadata
+ */
+package favsong;
+
+public class FavSong {
+
+    public static void main(String[] args) {
+        String artist = "Queen";
+     
+        String title = "Bohemian Rhapsody";
+        
+        System.out.println(artist.toUpperCase() );
+        System.out.println(title);
+        
+        int yearReleased = 1975;
+        
+        System.out.println(yearReleased);
+        
+        String genre = "progressive rock";
+        
+        System.out.println(genre);
+        
+        String quote = "duration";
+        
+        float duration = 5.55f;
+        
+        System.out.println(quote + " " + duration);
+        
+        
+        
+        
+/* math function - round & pow + chracters
+        
+        wordsInTotal = 333;
+
+        */
+        char letter1 = 87;
+        char letter2 = 111;
+        char letter3 = 114;
+        char letter4 = 100;
+        char letter5 = 115;
+        char letter6 = 61;
+        
+                System.out.println(letter1);
+                System.out.println(letter2);
+                System.out.println(letter3);
+                System.out.println(letter4);
+                System.out.println(letter5);
+                System.out.println(letter6);
+                
+                       
+
+        double words = 333.33;
+        words = java.lang.Math.round(words);
+        System.out.println(words);
+        
+        double inTotal = 333.0;
+        double number = 1;
+        inTotal = java.lang.Math.pow(inTotal, number);
+        System.out.println(inTotal);
+        
+                
+        
+        
+
+        
+        
+                
+    }
+    
+}
